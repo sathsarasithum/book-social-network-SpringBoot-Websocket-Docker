4165bd8c94badb3bc1ea2566790e1bc238cdf3a7
sathsarasithum <sathsaragms.21@uom.lk>
2026-05-04 18:44:52 +0530

    change the postgres sql port number 5432 -> 5433

--- book-network/src/main/java/com/sathsara/book/BookNetworkApplication.java
+++ book-network/src/main/java/com/sathsara/book/BookNetworkApplication.java
@@ -1,7 +1,11 @@
 package com.sathsara.book;
 
+import com.sathsara.book.role.Role;
+import com.sathsara.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,15 @@ public class BookNetworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
 	}
 
 }
--- book-network/src/main/java/com/sathsara/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/sathsara/book/auth/AuthenticationController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("auth")
