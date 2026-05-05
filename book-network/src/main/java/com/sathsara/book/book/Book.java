28633e32108371c52a21641910c29baac927ea41
sathsarasithum <sathsaragms.21@uom.lk>
2026-05-05 12:53:26 +0530

    create new book, feedback, book_transaction_history entity

---
+++ book-network/src/main/java/com/sathsara/book/book/Book.java
@@ -0,0 +1,44 @@
+package com.sathsara.book.book;
+
+import com.sathsara.book.common.BaseEntity;
+import com.sathsara.book.feedback.Feedback;
+import com.sathsara.book.history.BookTransactionHistory;
+import com.sathsara.book.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedBacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+
+}
---
+++ book-network/src/main/java/com/sathsara/book/feedback/Feedback.java
@@ -0,0 +1,27 @@
+package com.sathsara.book.feedback;
+
+import com.sathsara.book.book.Book;
+import com.sathsara.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+}
