4aaf7ad2eaba349a34ad100c08edd4c2e5097a57
sathsarasithum <sathsaragms.21@uom.lk>
2026-05-07 02:20:34 +0530

    create book service business functionality

--- book-network/src/main/java/com/sathsara/book/handler/GlobalExceptionHandler.java
+++ book-network/src/main/java/com/sathsara/book/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sathsara.book.handler;
 
+import com.sathsara.book.exception.OperationNotPermittedException;
 import jakarta.mail.MessagingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -79,16 +80,16 @@ public class GlobalExceptionHandler {
 //                );
 //    }
 
-//    @ExceptionHandler(OperationNotPermittedException.class)
-//    public ResponseEntity<ExceptionResponse> handleException(OperationNotPermittedException exp) {
-//        return ResponseEntity
-//                .status(BAD_REQUEST)
-//                .body(
-//                        ExceptionResponse.builder()
-//                                .error(exp.getMessage())
-//                                .build()
-//                );
-//    }
+    @ExceptionHandler(OperationNotPermittedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(OperationNotPermittedException exp) {
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(
+                        ExceptionResponse.builder()
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp) {
