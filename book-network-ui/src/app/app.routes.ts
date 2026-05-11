4742ef318a87d4fb3471e0ed01f90964e2308bc5
sathsarasithum <sathsaragms.21@uom.lk>
2026-05-11 18:41:54 +0530

    bug fix

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './pages/login/login';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- book-network-ui/src/app/pages/login/login.ts
+++ book-network-ui/src/app/pages/login/login.ts
@@ -12,37 +12,7 @@ export class LoginComponent implements OnInit {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsg: Array<string> = [];
 
-  constructor(
-    private ss: KeycloakService
-  ) {
-  }
+  login(){
 
-  async ngOnInit(): Promise<void> {
-    await this.ss.init();
-    await this.ss.login();
-  }
-
-  login() {
-    this.errorMsg = [];
-    this.authService.authenticate({
-      body: this.authRequest
-    }).subscribe({
-      next: (res) => {
-        this.tokenService.token = res.token as string;
-        this.router.navigate(['books']);
-      },
-      error: (err) => {
-        console.log(err);
-        if (err.error.validationErrors) {
-          this.errorMsg = err.error.validationErrors;
-        } else {
-          this.errorMsg.push(err.error.errorMsg);
-        }
-      }
-    });
-  }
-
-  register() {
-    this.router.navigate(['register']);
-  }
+    }
 }
